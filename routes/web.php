191a89b8cfc68f096deb9914335afa8601839443
AbdoelMadjid <abdulmadjid.mpd@gmail.com>
2025-08-18 01:08:25 +0700

    template velzon switch language

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,6 +12,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+//Language Translation
+Route::get('lang/switch/{lang}', function ($lang) {
+    Session::put('lang', $lang);
+    return redirect()->back();
+})->name('lang.switch');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
